2d5db3de2de79581d5059b5d39c1be16e5203d44
AlfreelinaGuzman <alfreelinaguzman@gmail.com>
2020-12-05 23:38:35 -0500

    se estan haciendo

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,9 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
            // Console.WriteLine("\nWhat is your name? ");
-var name = Console.ReadLine();
-var date = DateTime.Now;
-Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+//var name = Console.ReadLine();
+//var date = DateTime.Now;
+//Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
 //Console.Write("\nPress any key to exit...");
 Console.ReadKey(true);
         }
