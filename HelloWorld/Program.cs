229525ccc527bee1ec1dfc42ca24477b49675088
AlfreelinaGuzman <alfreelinaguzman@gmail.com>
2020-12-05 23:04:26 -0500

    fdgfg

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nWhat is your name? ");
+           // Console.WriteLine("\nWhat is your name? ");
 var name = Console.ReadLine();
 var date = DateTime.Now;
 Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
