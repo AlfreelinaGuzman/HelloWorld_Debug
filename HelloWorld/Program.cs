b4df3363563169b3fb43dfa1be39abdea2d2d0a9
AlfreelinaGuzman <alfreelinaguzman@gmail.com>
2020-12-05 23:15:09 -0500

    cxvf

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
            // Console.WriteLine("\nWhat is your name? ");
 var name = Console.ReadLine();
 var date = DateTime.Now;
-Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+//Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
 //Console.Write("\nPress any key to exit...");
 Console.ReadKey(true);
         }
